dc5880b519bf836074eaf0b0b1b4ef9c6b4a0cff
Max <81530826+TKSMax@users.noreply.github.com>
2021-08-04 17:05:18 -0700

    Create weather.py

---
+++ WeatherAPITing/weather.py
@@ -0,0 +1,26 @@
+import requests
+import os
+from datetime import datetime
+
+user_api = os.environ['current_weather_data']
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
